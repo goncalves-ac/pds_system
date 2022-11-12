8d03bf867bf67c3d088b314fc5f6b8a9106501a5
Leandro Souza <LMarqu23@its.jnj.com>
2022-11-12 12:22:30 -0300

    Adicionando validação para campos de entidades.

--- backend/dominio/servicos/CRUDImpl.ts
+++ backend/dominio/servicos/CRUDImpl.ts
@@ -8,11 +8,24 @@ export class CRUDImpl implements CRUD {
   private repo: Repositorio
   private entidadesUsuário: string[]
   private entidadesOrganizacionais: string[]
+  private camposCliente: string[]
+  private camposPsicologo: string[]
+  private camposSecretaria: string[]
+  private camposConsulta: string[]
+  private camposProntuario: string[]
 
+  // Todo: refatorar as declarações de campos e entidades para validação
   constructor(repo: Repositorio) {
     this.repo = repo
+    // Declaração dos nomes válidos para cada coleção de documento/tipos de entidade
     this.entidadesUsuário = ['clientes', 'psicologos', 'secretarias']
     this.entidadesOrganizacionais = ['consultas', 'prontuarios']
+    // Declaração dos campos válidos para cada tipo de entidade
+    this.camposCliente = ['nome', 'cpf', 'telefone', 'email', 'endereco']
+    this.camposPsicologo = ['nome', 'cpf', 'telefone', 'email', 'endereco', 'crp', 'workDays', 'especialidade']
+    this.camposSecretaria = ['nome', 'cpf', 'telefone', 'email', 'endereco', 'workDays', 'workHours']
+    this.camposConsulta = ['nomeCliente', 'nomePsicologo', 'dia', 'mes', 'ano', 'hora']
+    this.camposProntuario = ['nomeCliente', 'nomePsicologo', 'dia', 'mes', 'ano', 'parecer']
   }
 
   /**
@@ -29,12 +42,88 @@ export class CRUDImpl implements CRUD {
     return msgErro
   }
 
+  /**
+   * Funções para validação dos dados presentes nas rotas e requests.
+   */
   private validarTipoDeEntidade(entity_type: string): void {
     if (
       !this.entidadesUsuário.includes(entity_type) &&
       !this.entidadesOrganizacionais.includes(entity_type)
       ) {
         throw new Error('O tipo de entidade "' + entity_type + '" não existe. Favor passar na rota um tipo válido.');
+    }
+  }
+  private validarCamposDeEntidade(entity_type: string, dataObject: any): void {
+    let quantidadeDeCampos = 0;
+    Object.entries(dataObject).forEach(([chave, valor]) => {
+      this.validarChave(entity_type, chave)
+      this.validarTipos(entity_type, chave, valor)
+      quantidadeDeCampos++
+    })
+    this.validarQuantidadeDeCampos(entity_type, quantidadeDeCampos)
+  }
+  private validarChave(entity_type: string, chave: string): void {
+    const mensagemDeErro = 'Entidade do tipo:"' + entity_type + '" não possui campo com nome:"' + chave + '"'
+
+    if (entity_type === 'clientes' && !this.camposCliente.includes(chave)) {
+      throw new Error(mensagemDeErro);
+    }
+    if (entity_type === 'psicologos' && !this.camposPsicologo.includes(chave)) {
+      throw new Error(mensagemDeErro);
+    }
+    if (entity_type === 'secretarias' && !this.camposSecretaria.includes(chave)) {
+      throw new Error(mensagemDeErro);
+    }
+    if (entity_type === 'consultas' && !this.camposConsulta.includes(chave)) {
+      throw new Error(mensagemDeErro);
+    }
+    if (entity_type === 'prontuarios' && !this.camposProntuario.includes(chave)) {
+      throw new Error(mensagemDeErro);
+    }
+  }
+  private validarTipos(entity_type: string, chave:string, valor: any): void {
+    if (chave === 'endereco') {
+      if (!(typeof valor === 'object')) {
+        throw new Error('Entidade:"' + entity_type + '" deve ter campo:"' + chave + '" como tipo object e não "' + typeof valor + '"');
+      }
+      // Todo: Endereços são objetos. Logo faz sentido checar também os tipos de seus valores internos.
+      return
+    }
+    if (!(typeof valor === 'string')) {
+      throw new Error('Entidade:"' + entity_type + '" deve ter campo:"' + chave + '" como tipo string e não "' + typeof valor + '"');
+    }
+  }
+  // Todo: refatorar if statements, colocando-os numa única função.
+  private validarQuantidadeDeCampos(entity_type:string, quantidadeDeCampos: number) {
+    let qtdCampos = this.camposCliente.length
+    if (entity_type === 'clientes' && quantidadeDeCampos !== qtdCampos) {
+      throw new Error(
+        'Entidade:"' + entity_type + '" deve ter ( ' + qtdCampos + ' ) campos ao invés de ( ' + quantidadeDeCampos + ' )'
+      );
+    }
+    qtdCampos = this.camposPsicologo.length
+    if (entity_type === 'psicologos' && quantidadeDeCampos !== qtdCampos) {
+      throw new Error(
+        'Entidade:"' + entity_type + '" deve ter ( ' + qtdCampos + ' ) campos ao invés de ( ' + quantidadeDeCampos + ' )'
+      );
+    }
+    qtdCampos = this.camposSecretaria.length
+    if (entity_type === 'secretarias' && quantidadeDeCampos !== qtdCampos) {
+      throw new Error(
+        'Entidade:"' + entity_type + '" deve ter ( ' + qtdCampos + ' ) campos ao invés de ( ' + quantidadeDeCampos + ' )'
+      );
+    }
+    qtdCampos = this.camposConsulta.length
+    if (entity_type === 'consultas' && quantidadeDeCampos !== qtdCampos) {
+      throw new Error(
+        'Entidade:"' + entity_type + '" deve ter ( ' + qtdCampos + ' ) campos ao invés de ( ' + quantidadeDeCampos + ' )'
+      );
+    }
+    qtdCampos = this.camposProntuario.length
+    if (entity_type === 'prontuarios' && quantidadeDeCampos !== qtdCampos) {
+      throw new Error(
+        'Entidade:"' + entity_type + '" deve ter ( ' + qtdCampos + ' ) campos ao invés de ( ' + quantidadeDeCampos + ' )'
+      );
     }
   }
 
@@ -44,7 +133,7 @@ export class CRUDImpl implements CRUD {
   public adicionarEntidade(data: any, entity_type: string): any {
     try {
       this.validarTipoDeEntidade(entity_type)
-      // Todo: método que verifica estrutura dos dados, vendo se respeita a classe domínio correspondente
+      this.validarCamposDeEntidade(entity_type, data)
       return this.repo.addEntity(data, entity_type)
     } catch (error) {
       return this.tratarErro(error)
@@ -72,7 +161,7 @@ export class CRUDImpl implements CRUD {
   public atualizarEntidade(id: string, data: any, entity_type: string) {
     try {
       this.validarTipoDeEntidade(entity_type)
-      // Todo: método que verifica estrutura dos dados, vendo se respeita a classe domínio correspondente
+      this.validarCamposDeEntidade(entity_type, data)
       return this.repo.updateEntity(id, data, entity_type)
     } catch (error) {
       return this.tratarErro(error)
